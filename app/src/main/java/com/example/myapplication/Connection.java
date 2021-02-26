bd45e5d564669d308f8734e1a184376dbac342ce
Valen7inx <60442992+Valen7inx@users.noreply.github.com>
2021-02-26 12:14:00 +0100

    Connection

---
+++ app/src/main/java/com/example/myapplication/Connection.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+    import java.io.IOException;
+import java.net.*;
+    public class Connection {
+        public static void main(String[] args) throws IOException {
+            Socket socket = new Socket();
+            InetAddress inetAddress=InetAddress.getByName("localhost");
+            //the port should be greater or equal to 0, else it will throw an error
+            int port=1085;
+            //calling the constructor of the SocketAddress class
+            SocketAddress socketAddress=new InetSocketAddress(inetAddress, port);
+            //binding the  socket with the inetAddress and port number
+            socket.bind(socketAddress);
+            //connect() method connects the specified socket to the server
+            socket.connect(socketAddress);
+            System.out.println("Inet address: "+socket.getInetAddress());
+            System.out.println("Port number: "+socket.getLocalPort());
+    }
+}
