59e244c182e3b907d87552cc1ad0defa6bdaf519
Valen7inx <60442992+Valen7inx@users.noreply.github.com>
2021-03-03 12:15:29 +0100

    update

--- app/src/main/java/com/example/myapplication/Connection.java
+++ app/src/main/java/com/example/myapplication/Connection.java
@@ -1,19 +1,61 @@
 package com.example.myapplication;
+    import android.util.Log;
+
+    import java.io.BufferedWriter;
+    import java.io.DataOutputStream;
     import java.io.IOException;
-import java.net.*;
+    import java.io.InputStream;
+    import java.io.ObjectInputStream;
+    import java.io.OutputStream;
+    import java.io.OutputStreamWriter;
+    import java.io.PrintWriter;
+    import java.net.*;
+
+//Open a socket.
+//Open an input stream and output stream to the socket.
+//Read from and write to the stream according to the server's protocol.
+//Close the streams.
+//Close the socket.
+
     public class Connection {
-        public static void main(String[] args) throws IOException {
-            Socket socket = new Socket();
-            InetAddress inetAddress=InetAddress.getByName("localhost");
-            //the port should be greater or equal to 0, else it will throw an error
-            int port=1085;
-            //calling the constructor of the SocketAddress class
-            SocketAddress socketAddress=new InetSocketAddress(inetAddress, port);
-            //binding the  socket with the inetAddress and port number
-            socket.bind(socketAddress);
-            //connect() method connects the specified socket to the server
-            socket.connect(socketAddress);
-            System.out.println("Inet address: "+socket.getInetAddress());
-            System.out.println("Port number: "+socket.getLocalPort());
+        public Connection() {
+            try {
+                Socket socket = null;
+                //InetAddress inetAddress = InetAddress.getByName("40.68.217.34");
+                InetAddress inetAddress = InetAddress.getByName("192.168.1.129");
+                int port = 18000;
+
+                socket= new Socket(inetAddress, port);
+                Log.d("SocketDebug", "1");
+                String message= "Hello";
+
+                PrintWriter out = new PrintWriter(new BufferedWriter((new OutputStreamWriter(socket.getOutputStream()))), true);
+                out.println(message);
+                out.close();
+
+                OutputStream outputStream = socket.getOutputStream();
+                // create a data output stream from the output stream so we can send data through it
+                DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+                // write the message we want to send
+               // dataOutputStream.writeBytes("Hello");
+                //dataOutputStream.flush(); // send the message
+
+
+                //read the server response message
+                InputStream ois = socket.getInputStream();
+                int message1 = ois.read();
+                Log.d("SocketDebug", message+" 4");
+
+
+
+                System.out.println("Inet address: " + socket.getInetAddress());
+                System.out.println("Port number: " + socket.getLocalPort());
+                socket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.d("SocketDebug", "errore");
+            }
+
+        }
     }
-}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        Connection connection = new Connection();
+
     }
+
 }
