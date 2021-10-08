37bc069bb38cc649ace30fd21a39a31ef8b49752
Valen7inx <60442992+Valen7inx@users.noreply.github.com>
2021-10-08 12:37:48 +0200

    colors

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -73,6 +76,11 @@ public class HomeActivity extends AppCompatActivity {
 
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        //navView.setItemIconTintList(ColorStateList.valueOf(Color.parseColor("#3F51B5")));
+        //listen for navigation events
+        //avView.setListenerA(this);
+// select the correct nav menu item
+        //navView.getMenu().findItem(mNavItemId).setChecked(true);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
